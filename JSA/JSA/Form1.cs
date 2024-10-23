24581c0baf5e6ddb0db07b27fd61f22b85ad43c5
RipKip00765 <156421045+RipKip00765@users.noreply.github.com>
2024-10-23 13:14:50 +0200

    Main2

--- JSA/JSA/Form1.cs
+++ JSA/JSA/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.Json;
+
 
 namespace JSA
 {
@@ -15,6 +18,40 @@ namespace JSA
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonReadJson_Click(object sender, EventArgs e)
+        {
+            // Pad naar het JSON bestand
+            string jsonFilePath = @"C:\path\to\your\file\data.json";
+
+            // Controleer of het bestand bestaat
+            if (File.Exists(jsonFilePath))
+            {
+                try
+                {
+                    // Lees de inhoud van het JSON-bestand
+                    string jsonData = File.ReadAllText(jsonFilePath);
+
+                    // Deserializeer de JSON naar een C# object
+                    Person person = JsonSerializer.Deserialize<Person>(jsonData);
+
+                    // Toon de gegevens in de TextBox
+                    textBoxOutput.Text = $"Name: {person.Name}\r\nAge: {person.Age}\r\nEmail: {person.Email}";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Fout bij het lezen van JSON: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("JSON-bestand niet gevonden.");
+            }
         }
     }
 }
--- JSA/JSA/Properties/AssemblyInfo.cs
+++ JSA/JSA/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b53ac9c3-6174-4eba-add8-d1ca643713b3")]
+[assembly: Guid("96c1e270-646a-45c2-9462-452cea6f67dc")]
 
 // Version information for an assembly consists of the following four values:
 //
