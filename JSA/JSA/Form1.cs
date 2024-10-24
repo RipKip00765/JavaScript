6c3be85def8126bd589742d52e911e567a036802
RipKip00765 <156421045+RipKip00765@users.noreply.github.com>
2024-10-24 13:59:28 +0200

    REal

--- JSA/JSA/Form1.cs
+++ JSA/JSA/Form1.cs
@@ -9,49 +9,37 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Text.Json;
+using Newtonsoft.Json;
 
 
 namespace JSA
 {
     public partial class Form1 : Form
     {
+        public class Highscore
+        {
+            public string Score { get; set; }
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void ReadJSON()
         {
+            string FilePath = @"C:\Project\JS-Game\Score.json";
+
+            string json = File.ReadAllText(FilePath);
+
+            Highscore highscore = JsonConvert.DeserializeObject<Highscore>(json);
+
 
         }
-        private void buttonReadJson_Click(object sender, EventArgs e)
+
+        private void button1_Click(object sender, EventArgs e)
         {
-            // Pad naar het JSON bestand
-            string jsonFilePath = @"C:\path\to\your\file\data.json";
-
-            // Controleer of het bestand bestaat
-            if (File.Exists(jsonFilePath))
-            {
-                try
-                {
-                    // Lees de inhoud van het JSON-bestand
-                    string jsonData = File.ReadAllText(jsonFilePath);
-
-                    // Deserializeer de JSON naar een C# object
-                    Person person = JsonSerializer.Deserialize<Person>(jsonData);
-
-                    // Toon de gegevens in de TextBox
-                    textBoxOutput.Text = $"Name: {person.Name}\r\nAge: {person.Age}\r\nEmail: {person.Email}";
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Fout bij het lezen van JSON: {ex.Message}");
-                }
-            }
-            else
-            {
-                MessageBox.Show("JSON-bestand niet gevonden.");
-            }
+            ReadJSON();
         }
     }
 }
